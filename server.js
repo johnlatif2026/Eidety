54f2e86ed095caab5cb371cdb5a16baabc85d925
johnlatif2026 <johnlatif2026@gmail.com>
2026-04-08 22:27:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -82,8 +82,8 @@ app.get('/dashboard/data', authenticateToken, (req, res) => {
 });
 
 // ===== صفحات ثابتة =====
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));app.get('/login', (req, res) => {
-app.get('/login', (req, res) => {res.sendFile(path.join(__dirname, 'login.html'));});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'login.html')));
 app.get('/dashboard', authenticateToken, (req, res) => res.sendFile(path.join(__dirname, 'dashboard.html')));
 
 // ===== دالة حفظ البيانات =====
